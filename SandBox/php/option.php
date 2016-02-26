db3677a8a51e32e5cd0ff633ef4c38e31e7c162f
Emagu <n3np6ji39417@gmail.com>
2016-02-26 21:38:27 +0800

    更新設定

--- SandBox/php/option.php
+++ SandBox/php/option.php
@@ -1,15 +1,15 @@
 <?php
 //資料庫設定
 	//資料庫位置
-		$db_server = "localhost";
-		$db_passwd = "g3gp6ji39417";//管理者密碼
-		$db_user = "root";
-		$db_name = "sandbox";
+		$db_server = "mysql.hostinger.com.hk";
+		$db_passwd = "MDeWyJfa64";//管理者密碼
+		$db_user = "u336211189_sb";
+		$db_name = "u336211189_sb";
 //對資料庫連線
 function mysqlConnect(){
 	global $db_server, $db_user, $db_passwd,$db_name;
 	if(!@mysql_connect($db_server, $db_user, $db_passwd))
-        die($db_server.$db_user.$db_passwd);
+        die("Cann`t connect");
 
 	//資料庫連線採UTF8
 		mysql_query("SET NAMES utf8");
